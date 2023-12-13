aa90348fcc4431c4ea21c7753c2411bb3664e39e
Dharun297 <dharunpnb@gmail.com>
2023-12-12 23:38:57 -0800

    Schema Modified

--- src/dharun/dharun.dto.ts
+++
@@ -1,6 +0,0 @@
-export interface studentsDTO {
-    readonly name: String,
-    readonly age: Number,
-    readonly city: String,
-    readonly initial: String,
-}
--- src/dharun/schema.dharun.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-export const studentSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  city: String,
-  initial: String,
-});
